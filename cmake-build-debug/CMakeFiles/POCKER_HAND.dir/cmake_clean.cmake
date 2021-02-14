4f82cdfed580507749d0bd97a896c9c4aba6c1cf
maithaalhayyas <maithaalhayyas@gmail.com>
2021-02-14 14:54:00 +0400

    initial upload

---
+++ cmake-build-debug/CMakeFiles/POCKER_HAND.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POCKER_HAND.dir/main.c.o"
+  "POCKER_HAND"
+  "POCKER_HAND.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POCKER_HAND.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
